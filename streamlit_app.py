50588b860a83f9d9462e63df4d3672f598ffe173
Mariegri <147876376+Mariegri@users.noreply.github.com>
2025-07-08 15:02:32 +0000

    add res

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,22 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import cv2
 from ultralytics import YOLO
 
 st.set_page_config(layout="wide")
 st.title("Manga Translator")
 
 # Uploading the File to the Page
-uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+uploadedfile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
 #img_files = st.file_uploader(label="Choose an image files",
                  #type=['png', 'jpg', 'jpeg'],
                  #accept_multiple_files=True)
 
 # Checking the Format of the page
-if uploadFile is not None:
+if uploadedfile is not None:
     # Perform your Manupilations (In my Case applying Filters)
-    img = plt.imread(uploadFile)
+    img = plt.imread(uploadedfile)
     plt.axis('off')
     plt.imshow(img);
     st.image(img)
@@ -26,20 +27,48 @@ else:
     st.write("Make sure you image is in JPG/PNG Format.")
 
 
-st.subheader("Find text")
+    st.subheader("Find text")
 
-#model.load_state_dict(torch.load('data/best.pt'))
-model = YOLO('data/best.pt')
+    #model.load_state_dict(torch.load('data/best.pt'))
+    model = YOLO('data/best.pt')
 
-col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-with col1:
-    conf = st.slider("Conf", 0.0, 1.0, (0.2))
+    with col1:
+        conf = st.slider("Conf", 0.0, 1.0, (0.2))
 
-with col2:
-    iou = st.slider("IOU", 0.0, 1.0, (0.7))
+    with col2:
+        iou = st.slider("IOU", 0.0, 1.0, (0.7))
 
-res = model.predict(source = uploadFile, show = False, show_labels = False, save = False, conf = conf, iou = iou)
+    if uploadedfile is not None:
+        # Read image file
+        image_bytes = uploadedfile.getvalue()
+        orig_image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
 
-#im0 = run(source=open_cv_image, \
-#  conf_thres=0.25, weights="runs/detect/yolov7.pt")
+    if st.button("Get text", type="primary"):
+        res = model.predict(source = orig_image, show = False, show_labels = False, save = False, conf = conf, iou = iou)
+
+    st.write(res[0].boxes)
+
+    '''
+    # show yolo predictions with bboxes
+    def show_pics(image_path, bboxes):
+        
+        # read image
+        img = cv2.imread(image_path, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_h, img_w, can = img.shape
+        plt.figure(figsize = (20, 15))
+        fig, ax = plt.subplots()
+        plt.axis('off')
+
+        # add bboxes
+        for bbox in bboxes:
+            bbox = bbox.cpu().detach().numpy()
+            rectangle = mpl.patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], linewidth = 2, edgecolor = 'red', facecolor = 'none', lw = 2)
+            ax.add_patch(rectangle)
+            
+        plt.imshow(img)
+        plt.show();
+        mpl.pyplot.close();
+    '''
