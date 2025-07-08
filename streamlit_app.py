0fc74b8b8131a0d81a684cd0bc10184f9b626522
Mariegri <147876376+Mariegri@users.noreply.github.com>
2025-07-08 14:07:43 +0000

    add model

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,16 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from ultralytics import YOLO
 
 st.set_page_config(layout="wide")
 st.title("Manga Translator")
 
 # Uploading the File to the Page
 uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+#img_files = st.file_uploader(label="Choose an image files",
+                 #type=['png', 'jpg', 'jpeg'],
+                 #accept_multiple_files=True)
 
 # Checking the Format of the page
 if uploadFile is not None:
@@ -20,3 +24,23 @@ if uploadFile is not None:
     #st.write("Image Uploaded Successfully")
 else:
     st.write("Make sure you image is in JPG/PNG Format.")
+
+
+st.subheader("Find text")
+
+#model.load_state_dict(torch.load('data/best.pt'))
+#model = YOLO('data/best.pt')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    conf = st.slider("Conf", 0.0, 1.0, (0.2))
+
+with col2:
+    iou = st.slider("IOU", 0.0, 1.0, (0.7))
+
+#res = model.predict(source = uploadFile, show = False, show_labels = False, save = False,
+                    #conf = conf, iou = iou)
+
+#im0 = run(source=open_cv_image, \
+#  conf_thres=0.25, weights="runs/detect/yolov7.pt")
