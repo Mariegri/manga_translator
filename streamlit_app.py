78da7ba40d677747b86946bcc5ab39e6033999c3
Mariegri <147876376+Mariegri@users.noreply.github.com>
2025-07-08 13:21:56 +0000

    add test model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(layout="wide")
+st.title("Manga Translator")
+
+# Uploading the File to the Page
+uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+
+# Checking the Format of the page
+if uploadFile is not None:
+    # Perform your Manupilations (In my Case applying Filters)
+    img = plt.imread(uploadFile)
+    plt.axis('off')
+    plt.imshow(img);
+    st.image(img)
+    #st.write("Image Uploaded Successfully")
+else:
+    st.write("Make sure you image is in JPG/PNG Format.")
