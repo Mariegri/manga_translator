1e9cdc1881ad149c39c44cb3c3b7ca36c9f81a84
Mariegri <147876376+Mariegri@users.noreply.github.com>
2025-07-08 16:14:42 +0000

    add df

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,47 +10,53 @@ st.set_page_config(layout="wide")
 st.title("Manga Translator")
 stage = 0
 
-# Uploading the File to the Page
-uploadedfile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
-#img_files = st.file_uploader(label="Choose an image files",
-                 #type=['png', 'jpg', 'jpeg'],
-                 #accept_multiple_files=True)
+# Stage 0: upload image
+#uploadedfile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+uploadedfile = st.file_uploader(label = "You can choose up to 13 image files", type=['png', 'jpg', 'jpeg'], accept_multiple_files = True)
+if len(uploadedfile) > 13:
+    st.write("You can choose only up to 13 images")
+else:
+    images_df = pd.DataFrame(columns = [uploadedfile[i] for i in range(len(uploadedfile))])
 
-# Checking the Format of the page
+
+
+st.write(len(uploadedfile))
+
+img = plt.imread(uploadedfile[2])
+plt.axis('off')
+plt.imshow(img);
+st.image(img)
 
 if uploadedfile is not None:
-    # Perform your Manupilations (In my Case applying Filters)
     img = plt.imread(uploadedfile)
     plt.axis('off')
     plt.imshow(img);
     st.image(img)
-    #st.write("Image Uploaded Successfully")
     stage = 1
 else:
     st.write("Make sure you image is in JPG/PNG Format.")
 
+# Stage 1: get bboxes using pretrained yolo-model
 if stage > 0:
     st.subheader("Find text")
-
-    #model.load_state_dict(torch.load('data/best.pt'))
     model = YOLO('data/best.pt')
 
+    # add conf and iou changers
     col1, col2 = st.columns(2)
-
     with col1:
         conf = st.slider("Conf", 0.0, 1.0, (0.2))
-
     with col2:
-        iou = st.slider("IOU", 0.0, 1.0, (0.7))
+        iou = st.slider("IOU", 0.0, 1.0, (0.7))  
 
-    if uploadedfile is not None:
-        # Read image file
+    # get predictions with the model
+    if st.button("Get text", type="primary"):
         image_bytes = uploadedfile.getvalue()
         orig_image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
-
-    if st.button("Get text", type="primary"):
         res = model.predict(source = orig_image, show = False, show_labels = False, save = False, conf = conf, iou = iou)
+        stage = 2
 
+# Stage 2: show and adjust bboxes
+if stage > 1:
     st.write(res[0].boxes)
 
 '''
